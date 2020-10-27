6dbfe0919f7017fb127bf6ac6d05dcdfc703023f
issamlammri <issamlamri34000@gmail.com>
2020-10-27 10:56:02 +0100

    add the first page

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 "# IDE-WEB-G404" 
+
+
+Pour instaler le BU de mdboostrap
+
+First you have to install Node.js and also Yarn or NPM. When you’re ready you can install encore by running on your symfony root project:
+$ yarn add @symfony/webpack-encore --dev
+
+or using npm
+
+$ npm init
+$ npm install @symfony/webpack-encore --save-dev
---
+++ src/Controller/FirstPage.php
@@ -0,0 +1,38 @@
+<?php
+//src/Controller/FirstPage.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment; 
+
+class FirstPage
+{
+    
+    /**
+     *@var Environment
+     */
+     private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+        
+        return new Response(
+            '<html>
+                <body>
+                    '.$number.'
+                    <br>
+                    <br>
+                    On a ajouter la premier page 
+                </body>
+             </html>'
+        );
+        
+
+        //return new Response($this->twig-render('page/home.html.twig'));
+    }
+}
